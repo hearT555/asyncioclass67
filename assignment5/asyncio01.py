f59351803c1ce6b74a5e71525d21cdec9290e453
RawipasP <139108698+RawipasP@users.noreply.github.com>
2024-07-10 12:07:33 +0700

    submit assignment 5

--- assignment5/asyncio01.py
+++ assignment5/asyncio01.py
@@ -1,1 +1,24 @@
 # example of waiting for all tasks to complete
+from random import random
+import asyncio
+
+#coroutine to execute  in a new task
+async def task_coro(arg):
+    #generate a random value brtween 0 and 1
+    value = random
+    #block for a moment
+    await asyncio.sleep(value)
+    #report he value
+    print(f">task {arg} done with {value}")
+
+#main  n coroutine
+async def main():
+    #create many tasks
+    tasks = [asyncio.create_task(task_coro(i)) for i in range(10)]
+    #wait for all tasks to complete
+    done,pending = await asyncio.wait(tasks)
+    #report results
+    print("All done")
+
+#start the asyncio program
+asyncio.run(main())
